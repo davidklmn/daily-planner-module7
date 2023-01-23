03910c1fde1aabaa6e9b33cd3393f95c033fb454
davidklmn <davidkelemen88@gmail.com>
2023-01-23 15:54:33 +0000

    Generated rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,42 @@
 // ? Display the current day at the top of the calendar when a user opens the planner.
 let currentDate = moment().format("dddd, Do [of] MMMM");
 let dateDisplay = $("#currentDay");
+
+// * Display current date
 dateDisplay.append(currentDate);
 
 // ? Present timeblocks for standard business hours when the user scrolls down.
-let mainContainer = $(".container");
-let hoursRow = $("<div>");
-let hour = $("<p>");
-let textContainer = $("<textarea>");
-let saveButton = $("<button>");
-let saveIcon = $("<i>");
+$(function () {
+  for (let i = 9; i < 18; i++) {
+    let mainContainer = $(".container");
+    let hoursRow = $("<div>");
+    let hour = $("<p>");
+    let textContainer = $("<textarea>");
+    let saveButton = $("<button>");
+    let saveIcon = $("<i>");
 
-// * Create 'row' container
-hoursRow.addClass("time-block row");
-mainContainer.append(hoursRow);
+    // * Classes
+    hoursRow.addClass("time-block row");
+    hour.addClass("hour col-1");
+    textContainer.addClass("textarea col-10");
+    saveIcon.addClass("fa-solid fa-floppy-disk");
+    saveButton.addClass("saveBtn col-1");
 
-// * Add hours
-hour.addClass("hour col-1");
-hour.text("1pm");
-hoursRow.append(hour);
+    // * Create 'row' container
+    mainContainer.append(hoursRow);
 
-// * Add input textarea
-textContainer.addClass("textarea col-10");
-hoursRow.append(textContainer);
+    // * Add hours
+    hour.text(i + "PM");
+    hoursRow.append(hour);
 
-// * Add button
-saveIcon.addClass("fa-solid fa-floppy-disk");
-saveButton.append(saveIcon);
-saveButton.addClass("saveBtn col-1");
-hoursRow.append(saveButton);
+    // * Add input textarea
+    hoursRow.append(textContainer);
+
+    // * Add button
+    saveButton.append(saveIcon);
+    hoursRow.append(saveButton);
+  }
+});
 
 // ? Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 
